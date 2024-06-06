544ff033843407ec86031efb787ae1b513058b45
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-06-06 13:03:29 -0500

    Get pokemon data by id

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,11 +1,28 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
   params: { id: string }
 }
 
-export default function PokemonPage({ params }: Props) {  
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache' // TODO: Cambiar después
+  })
+    .then(res => res.json())
+    
+  return pokemon
+}
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id)
+
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+
+      <div>
+        {JSON.stringify(pokemon)}
+      </div>
     </div>
   );
 }
