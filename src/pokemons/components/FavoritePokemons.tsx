2e95fb264d51cdc5db2b46290bab8cd1efc5da6f
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-07-03 12:46:41 -0500

    No favorites logic

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -1,17 +1,34 @@
 'use client'
 
+import { useState } from "react"
 import { useAppSelector } from "@/store"
+import { IoHeartOutline } from "react-icons/io5"
 
 import { PokemonGrid } from "./PokemonGrid"
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector(state => Object.values(state.pokemons))
-  
+  const [pokemons, setPokemons] = useState(favoritePokemons);
+
   return (
     <div className="flex flex-col">
       <span className="text-5xl my-2">Pokemons favoritos<small className="text-blue-500">Global state</small></span>
       
-      <PokemonGrid pokemons={favoritePokemons} />
+      {/* <PokemonGrid pokemons={favoritePokemons} /> */}
+      {
+        pokemons.length > 0
+        ? <PokemonGrid pokemons={pokemons} />
+        : <NoFavorites />
+      }
+    </div>
+  )
+}
+
+const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[50vh] items-center justify-center">
+      <IoHeartOutline size={100} className="text-red-500" />
+      <span>No hay favoritos</span>
     </div>
   )
 }
--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -6,9 +6,9 @@ interface PokemonsState {
 }
 
 const initialState: PokemonsState = {
-  '1': { id: '1', name: 'bulbasaur' },
-  '4': { id: '4', name: 'charmander' },
-  '7': { id: '7', name: 'squirtle' },
+  // '1': { id: '1', name: 'bulbasaur' },
+  // '4': { id: '4', name: 'charmander' },
+  // '7': { id: '7', name: 'squirtle' },
 }
 
 const pokemonsSlice = createSlice({
