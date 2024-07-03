b92ebfc7659b49c11566ba0fbfa3f36c25f36ce1
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-07-03 13:22:34 -0500

    Create middleware for localStorage

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -5,10 +5,13 @@ interface PokemonsState {
   [key: string]: SimplePokemon
 }
 
+const getInitialState = (): PokemonsState => {
+  const favorites = JSON.parse(localStorage.getItem('favorite-pokemons') ?? '{}')
+  return favorites
+}
+
 const initialState: PokemonsState = {
-  // '1': { id: '1', name: 'bulbasaur' },
-  // '4': { id: '4', name: 'charmander' },
-  // '7': { id: '7', name: 'squirtle' },
+  ...getInitialState() 
 }
 
 const pokemonsSlice = createSlice({
@@ -22,10 +25,14 @@ const pokemonsSlice = createSlice({
 
       if (!!state[id]) {
         delete state[id]
-        return
+        // return
+      } else {
+        state[id] = pokemon
       }
 
-      state[id] = pokemon
+      //TODO: No se debe hacer en Redux, mala práctica
+      // localStorage.setItem('favorite-pokemons', JSON.stringify(state))
+
     }
   }
 });
